20e7afcd484e8cedd5901727a7b813a5be7b02cd
Raubinsh Arya <raubinsha@adobe.com>
2022-09-16 15:27:43 +0530

    [Graph] Detect cycle in undirected graph using BFS

---
+++ src/Graph/DetectCycleUndirectedGraph.java
@@ -0,0 +1,41 @@
+package Graph;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Problem Link
+// https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+
+public class DetectCycleUndirectedGraph {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[] = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (checkCycle(adj, visited, i)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkCycle(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int src) {
+        visited[src] = true;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair<>(src, -1));
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> pair = queue.poll();
+            int node = pair.row;
+            int parent = pair.col;
+            for (int i : adj.get(node)) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.offer(new Pair<>(i, node));
+                } else if (parent != i) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
