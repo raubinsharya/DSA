c1882e8e1ebb34665c63fd61fa96b37a051f5e00
Raubinsh Arya <raubinsha@adobe.com>
2022-10-18 17:23:10 +0530

    [DP] Frog Jump with TopDown and Bottom up and with space optimization

--- src/DP/FrogJump.java
+++ src/DP/FrogJump.java
@@ -7,16 +7,47 @@ package DP;
 public class FrogJump {
     public static int frogJump(int n, int heights[]) {
         int dp[] = new int[n + 1];
-        return solve(n, dp, heights);
+        return solveBottomUp(n, heights);
+        // return solveTopDown(n - 1, dp, heights);
     }
 
-    public static int solve(int n, int[] dp, int[] heights) {
+
+    // Top-down approach
+    public static int solveTopDown(int n, int[] dp, int[] heights) {
         if (n == 0) return 0;
         if (dp[n] != 0) return dp[n];
-        int one = solve(n - 1, dp, heights) + Math.abs(heights[n] - heights[n - 1]);
+        int one = solveTopDown(n - 1, dp, heights) + Math.abs(heights[n] - heights[n - 1]);
         int two = Integer.MAX_VALUE;
         if (n > 1)
-            two = solve(n - 2, dp, heights) + Math.abs(heights[n] - heights[n - 2]);
+            two = solveTopDown(n - 2, dp, heights) + Math.abs(heights[n] - heights[n - 2]);
         return dp[n] = Math.min(one, two);
     }
+
+    // Bottom-Up approach without space optimization
+//    private static int solveBottomUp(int n, int[] heights) {
+//        int dp[] = new int[n + 1];
+//        dp[0] = 0;
+//        for (int i = 1; i < n; i++) {
+//            int one = dp[i - 1] + Math.abs(heights[i] - heights[i - 1]);
+//            int two = Integer.MAX_VALUE;
+//            if (i > 1) two = dp[i - 2] + Math.abs(heights[i] - heights[i - 2]);
+//            dp[i] = Math.min(one, two);
+//        }
+//        return dp[n - 1];
+//    }
+
+    // Bottom-Up approach with space optimization
+    private static int solveBottomUp(int n, int[] heights) {
+        int first = 0;
+        int second = 0;
+        for (int i = 1; i < n; i++) {
+            int one = first + Math.abs(heights[i] - heights[i - 1]);
+            int two = Integer.MAX_VALUE;
+            if (i > 1) two = second + Math.abs(heights[i] - heights[i - 2]);
+            second = first;
+            first = Math.min(one, two);
+        }
+        return first;
+    }
+
 }
