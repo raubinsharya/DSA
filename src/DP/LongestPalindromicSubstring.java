56c6421e089b1a83499fbed75ecffd0c12f5394d
Raubinsh Arya <raubinsha@adobe.com>
2022-11-05 23:41:37 +0530

    [DP] Minimum Insertion to make a string palindrome

--- src/DP/LongestPalindromicSubstring.java
+++ src/DP/LongestPalindromicSubstring.java
@@ -1,20 +1,25 @@
 package DP;
-// https://leetcode.com/problems/longest-palindromic-subsequence/description/
-public class LongestPalindromicSubsequence {
-    public int longestPalindromeSubseq(String s1) {
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s1) {
         String s2 = new StringBuilder(s1).reverse().toString();
         int m = s1.length();
         int n = s2.length();
+        int maxi = 0;
+        int end = 0;
         int dp[][] = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 0;
+                    if (maxi <= dp[i][j]) {
+                        maxi = dp[i][j];
+                        end = i;
+                    }
                 }
             }
         }
-        return dp[m][n];
+
+        return s1.substring(maxi - end, end);
     }
 }
--- src/DP/MinimumInsertionToMakeStringPalindrome.java
+++ src/DP/MinimumInsertionToMakeStringPalindrome.java
@@ -1,17 +1,25 @@
 package DP;
-// https://leetcode.com/problems/longest-palindromic-subsequence/description/
-public class LongestPalindromicSubsequence {
-    public int longestPalindromeSubseq(String s1) {
+
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+
+public class MinimumInsertionToMakeStringPalindrome {
+    public int minInsertions(String s) {
+        int lcs = longestPalindromeSubseq(s);
+        return s.length() - lcs;
+    }
+
+    public static int longestPalindromeSubseq(String s1) {
         String s2 = new StringBuilder(s1).reverse().toString();
         int m = s1.length();
         int n = s2.length();
+        if (s1.equals(s2)) return m;
         int dp[][] = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = 0;
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
