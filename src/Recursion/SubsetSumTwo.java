32220e79dd27c0b96049a7f72249c64492279f17
Raubinsh Arya <raubinsha@adobe.com>
2022-12-05 19:21:57 +0530

    [Recursion] Subsets ii

--- src/Recursion/SubsetSumTwo.java
+++ src/Recursion/SubsetSumTwo.java
@@ -1,4 +1,28 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+// https://leetcode.com/problems/subsets-ii/description/
 public class SubsetSumTwo {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Integer> ds = new LinkedList<>();
+        res.add(new ArrayList<>());
+        solve(nums, 0, nums.length, ds, res);
+        return res;
+    }
+
+    private void solve(int[] nums, int idx, int n, LinkedList<Integer> ds, List<List<Integer>> res) {
+        for (int i = idx; i < n; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            ds.add(nums[i]);
+            res.add(new ArrayList<>(ds));
+            solve(nums, i + 1, n, ds, res);
+            ds.removeLast();
+        }
+    }
 }
