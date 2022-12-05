4539be3ed1b3657619a29e64a0ec842c1a41eac3
Raubinsh Arya <raubinsha@adobe.com>
2022-12-05 11:13:01 +0530

    [Recursion] Fibonacci Series

---
+++ src/Recursion/FibonacciSeries.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class FibonacciSeries {
+
+    public int fib(int n) {
+        return solveTopDown(n);
+    }
+
+    private int solveTopDown(int n) {
+        if (n == 1) return 1;
+        if (n < 1) return 0;
+        return solveTopDown(n - 1) + solveTopDown(n - 2);
+    }
+}
