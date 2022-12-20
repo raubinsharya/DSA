d5c958976f19074c855f5e58d80fe6d669ff58f0
Raubinsh arya <raubinsharya@gmail.com>
2022-12-20 19:11:32 +0530

    [General] Check if every row and columns contains all numbers

--- src/General/CheckEveryRowAndColContainsAllNumbers.java
+++ src/General/CheckEveryRowAndColContainsAllNumbers.java
@@ -1,2 +1,23 @@
-package General;public class CheckEveryRowAndColContainsAllNumbers {
+package General;
+
+import java.util.HashSet;
+import java.util.Set;
+// https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/description/
+public class CheckEveryRowAndColContainsAllNumbers {
+    public boolean checkValid(int[][] matrix) {
+        return solve(matrix);
+    }
+
+    private boolean solve(int[][] matrix) {
+        int n = matrix.length;
+        for (int row = 0; row < n; row++) {
+            Set<Integer> rows = new HashSet<>();
+            Set<Integer> cols = new HashSet<>();
+            for (int col = 0; col < n; col++) {
+                if (!rows.add(matrix[row][col])) return false;
+                if (!cols.add(matrix[col][row])) return false;
+            }
+        }
+        return true;
+    }
 }
