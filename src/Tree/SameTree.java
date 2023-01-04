898fbad5cb6c3a9b1245eb54932aab246c47bf3e
Raubinsh arya <raubinsharya@gmail.com>
2023-01-04 21:09:34 +0530

    [Tree] Same Tree

---
+++ src/Tree/SameTree.java
@@ -0,0 +1,12 @@
+package Tree;
+
+import Utils.TreeNode;
+// https://leetcode.com/problems/same-tree/description/
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
