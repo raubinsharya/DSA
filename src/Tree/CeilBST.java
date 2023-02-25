956a39fb0a50a0732df79a86ba65fd18a478cc40
Raubinsh arya <raubinsharya@gmail.com>
2023-02-25 23:33:37 +0530

    [Tree] Ceil in BST

---
+++ src/Tree/CeilBST.java
@@ -0,0 +1,26 @@
+package Tree;
+
+final class TreeNode<T> {
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+
+    TreeNode(T data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+};
+
+// https://www.codingninjas.com/codestudio/problems/ceil-from-bst_920464
+public class CeilBST {
+    public static int findCeil(TreeNode<Integer> root, int x) {
+        if (root == null) return -1;
+        int left = findCeil(root.left, x);
+        if (left > -1) return left;
+        if (root.data >= x) return root.data;
+        int right = findCeil(root.right, x);
+        if (right > -1) return right;
+        return -1;
+    }
+}
